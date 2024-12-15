556783884555bebf8e0e57f4c8ca692f6dd5e365
Shaharia <shahariahossain72@gmail.com>
2024-12-15 13:08:55 +0600

    review model done

--- doctor_app/models.py
+++ doctor_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from patient.models import Patient
 
 class Specialization(models.Model):
     name = models.CharField(max_length=20)
@@ -27,7 +27,7 @@ class AvailableTime(models.Model):
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(upload_to="doctor/images/") 
+    image = models.ImageField(upload_to="doctor_app/images/") 
     designation = models.ForeignKey(Designation,on_delete=models.CASCADE)
     specialization = models.ManyToManyField(Specialization)
     availableTime = models.ManyToManyField(AvailableTime)
@@ -36,3 +36,22 @@ class Doctor(models.Model):
 
     def __str__(self):
         return f"Dr. {self.user.first_name} {self.user.last_name}"
+
+
+STAR_CHOICES = [
+    ('★','★'),
+    ('★★','★★'),
+    ('★★★','★★★'),
+    ('★★★★','★★★★'),
+    ('★★★★★','★★★★★'),
+]
+
+class Review(models.Model):
+    reviewer = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    body = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    rating = models.CharField(choices=STAR_CHOICES,max_length=20)
+
+    def __str__(self):
+        return f"Patient: {self.reviewer.user.first_name} ; Doctor: {self.doctor.user.first_name}"
