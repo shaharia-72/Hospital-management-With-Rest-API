65a1d4d20ed048a74d898fa509071f3d778cc85b
Jasmin1211 <jasminakter27716@gmail.com>
2024-12-19 11:08:01 +0600

    CROS problem solverd agian 0v.1

--- BackendHospital/settings.py
+++ BackendHospital/settings.py
@@ -30,25 +30,6 @@ SECRET_KEY = 'django-insecure-gou=*baa-4f-!b%(l1!ny-te&g7$c)0-3xqd!5t$sn!m@)%9(^
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-CSRF_TRUSTED_ORIGINS = [
-    'https://*,mydomain.com',
-    'https://*.127.0.0.1',
-    'https://hospital-management-with-rest-api.onrender.com',
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-]
-
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "https://hospital-management-with-rest-api.onrender.com",
-]
-
-
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    "https://.*\.yourdomain\.com$",
-]
 
 
 
@@ -89,6 +70,24 @@ CORS_ALLOW_HEADERS = [
     'content-type',
     'authorization',
     'x-csrftoken',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "*",
+    'https://*,mydomain.com',
+    'https://*.127.0.0.1',
+    'https://hospital-management-with-rest-api.onrender.com',
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "*",
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    "https://hospital-management-with-rest-api.onrender.com",
 ]
 
 TEMPLATES = [
@@ -178,7 +177,3 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env("EMAIL")
 EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")  # Your App Password
 DEFAULT_FROM_EMAIL = 'noreplay@gmail'
-
-
-
-
