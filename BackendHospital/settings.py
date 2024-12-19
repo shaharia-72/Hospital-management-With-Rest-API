1c88f41c17d1ff31c5e27ed12a3c299a8c6af7ae
Jasmin1211 <jasminakter27716@gmail.com>
2024-12-19 11:33:24 +0600

    remove * for making problem in deployment

--- BackendHospital/settings.py
+++ BackendHospital/settings.py
@@ -73,12 +73,11 @@ CORS_ALLOW_HEADERS = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGIN_REGEXES = [
-    'r"^https://\w+\.example\.com$"',  # Regex pattern for allowed subdomains
-]
+# CORS_ALLOWED_ORIGIN_REGEXES = [
+#     'r"^https://\w+\.example\.com$"',  # Regex pattern for allowed subdomains
+# ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://*:*.com',
     'https://*.127.0.0.1',
     'http://*.127.0.0.1',
     'https://hospital-management-with-rest-api.onrender.com',
@@ -95,7 +94,6 @@ CSRF_TRUSTED_ORIGINS = [
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "*",
     "http://127.0.0.1:5500",
     "http://127.0.0.1:5500",
     "http://localhost:5500",
