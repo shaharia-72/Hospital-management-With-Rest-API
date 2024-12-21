0c5f1687e92914c58bee05aa47a5ad415bfdc41b
Jasmin1211 <jasminakter27716@gmail.com>
2024-12-21 13:21:17 +0600

    updated in doctor sections

--- doctor_app/serializers.py
+++ doctor_app/serializers.py
@@ -36,13 +36,21 @@ class DoctorSerializer(serializers.ModelSerializer):
         return None
 
 class DoctorReviewSerializer(serializers.ModelSerializer):
-    user  = serializers.StringRelatedField(many=False)
+    # Existing fields
+    user = serializers.StringRelatedField(many=False)
     reviewer_full_name = serializers.SerializerMethodField()
+    doctor_full_name = serializers.SerializerMethodField()  # New field
+
     class Meta:
         model = models.Review
-        fields = '__all__'
+        fields = '__all__'  
 
     def get_reviewer_full_name(self, obj):
         if obj.reviewer and obj.reviewer.user:
             return f"{obj.reviewer.user.first_name} {obj.reviewer.user.last_name}"
+        return None
+
+    def get_doctor_full_name(self, obj):
+        if obj.doctor and obj.doctor.user:
+            return f"{obj.doctor.user.first_name} {obj.doctor.user.last_name}"
         return None
