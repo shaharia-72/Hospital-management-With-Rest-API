1cb001751d0ce3a9b55d9cbe1ac5b8c3cd6c689b
Jasmin1211 <jasminakter27716@gmail.com>
2024-12-21 22:51:21 +0600

    version 1.0 confirm

--- users/views.py
+++ users/views.py
@@ -1,7 +1,18 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()  
+        user_id = self.request.query_params.get('user_id', None)  
+
+        if user_id:
+            queryset = queryset.filter(id=user_id)
+
+        return queryset
